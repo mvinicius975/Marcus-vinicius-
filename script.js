54067356a5f5095884295638b27321d641c26828
mvinicius975 <ferreira.lima.marcus@escola.pr.gov.br>
2024-10-16 16:18:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//Elementos do DOM
+const musicForm = document.getElementByld('musicForm');
+const musicTableBody = document.querySelector('#musicTable tBody');
+const exportBtn = document.getElementByld('exportBtn');
+
